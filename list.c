1d8af9c0e7c6ba93c8a885ac2266bb6af8ef9fd0
Replit user <>
2023-03-25 20:26:08 +0000

    new try

--- list.c
+++ list.c
@@ -64,6 +64,7 @@ void * lastList(List * list) {
 
 void * prevList(List * list) {
   if ( list->head == NULL) return NULL;
+  if( list->current == NULL) return NULL;
   if ( list->current = list->head) return NULL;
   
   
